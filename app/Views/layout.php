2488c58a835df92bd6a419841af434772bd565b0
Yusuf <ybasori@gmail.com>
2022-03-20 03:59:56 +0700

    icon

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -3,8 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#000000" />
     <title><?= !empty($title) ? $title : "" ?></title>
 
 
@@ -17,6 +19,8 @@
         <?php endforeach; ?>
 
     <?php endif; ?>
+    <link rel="apple-touch-icon" href="/logo192.png" />
+    <link rel="manifest" href="/manifest.json" />
 
 
     <style>
