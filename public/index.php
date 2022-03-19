aa3c3a5af0b907ff68d303925853915335db5512
Yusuf <ybasori@gmail.com>
2022-03-19 14:36:26 +0700

    update index.php

--- public/index.php
+++ public/index.php
@@ -4,21 +4,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set('UTC');
 
-print_r($_ENV['mode']);
-// if (file_exists(__DIR__ . '/../.env')) {
-//     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-//     $dotenv->load();
-// }
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-// $dotenv->load();
+error_reporting(E_ALL);
 
-// error_reporting(E_ALL);
+ini_set("display_errors", "0");
 
-// ini_set("display_errors", "0");
-
-// register_shutdown_function([\System\HandleError::onError(), "onError"]);
+register_shutdown_function([\System\HandleError::onError(), "onError"]);
 
 
-// require_once __DIR__ . '/../system/Functions.php';
-// require_once __DIR__ . '/../app/routes.php';
+require_once __DIR__ . '/../system/Functions.php';
+require_once __DIR__ . '/../app/routes.php';
