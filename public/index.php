a3d65597f0f457905272802b9a1493c586672300
Yusuf <ybasori@gmail.com>
2022-03-19 14:26:00 +0700

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,17 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-date_default_timezone_set('UTC');
+// date_default_timezone_set('UTC');
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 
-ini_set("display_errors", "0");
+// ini_set("display_errors", "0");
 
-register_shutdown_function([\System\HandleError::onError(), "onError"]);
+// register_shutdown_function([\System\HandleError::onError(), "onError"]);
 
 
-require_once __DIR__ . '/../system/Functions.php';
-require_once __DIR__ . '/../app/routes.php';
+// require_once __DIR__ . '/../system/Functions.php';
+// require_once __DIR__ . '/../app/routes.php';
